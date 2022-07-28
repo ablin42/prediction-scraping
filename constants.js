9cb357d92d166b847af44dcf4c214d7803720bb9
ablin <space6fic@gmail.com>
2022-07-28 22:20:30 +0200

    removed useless export

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 const BINANCE_API = "https://api.binance.com/api/v3";
 const AVERAGE_ID = "60d3836b82b6dfd7f7b04c53"; // ! needs to be spoofed or find a more secure approach
 
-module.exports = { RANGE_OPTIONS, BINANCE_API, AVERAGE_ID };
+module.exports = { BINANCE_API, AVERAGE_ID };
